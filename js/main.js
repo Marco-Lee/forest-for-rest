18931cfd914a88d8cd3c7672911549e77655b8c6
Marco-Lee <marco000703@naver.com>
2024-02-26 16:46:28 +0900

    main page

--- js/main.js
+++ js/main.js
@@ -1,11 +1,42 @@
-const menu1=document.querySelector(".navi>li>a");
-console.log(menu1);
-const mnpage=document.querySelector(".menu-page");
-const clbtn=document.querySelector(".close-btn");
+$(document).ready(function(){
+    $(".main1").on("click",function(){
+        $(".menu-page2").removeClass("active");
+        $(".menu-page3").removeClass("active");
+        $(".menu-page4").removeClass("active");
+        $(".menu-page1").addClass("active");
+    })
+    $(".close-btn1").on("click",function(){
+        $(".menu-page1").removeClass("active");
+    })
 
-menu1.addEventListener("click",()=>{
-    mnpage.classList.add("active");
-},500);
-clbtn.addEventListener("click",()=>{
-    mnpage.classList.remove("active");
-},500);
+    $(".main2").on("click",function(){
+        $(".menu-page1").removeClass("active");
+        $(".menu-page3").removeClass("active");
+        $(".menu-page4").removeClass("active");
+        $(".menu-page2").addClass("active");
+    })
+    $(".close-btn2").on("click",function(){
+        $(".menu-page2").removeClass("active");
+    })
+
+    $(".main3").on("click",function(){
+        $(".menu-page1").removeClass("active");
+        $(".menu-page2").removeClass("active");
+        $(".menu-page4").removeClass("active");
+        $(".menu-page3").addClass("active");
+    })
+    $(".close-btn3").on("click",function(){
+        $(".menu-page3").removeClass("active");
+    })
+
+    $(".main4").on("click",function(){
+        $(".menu-page1").removeClass("active");
+        $(".menu-page2").removeClass("active");
+        $(".menu-page3").removeClass("active");
+        $(".menu-page4").addClass("active");
+    })
+    $(".close-btn4").on("click",function(){
+        $(".menu-page4").removeClass("active");
+    })
+
+});
