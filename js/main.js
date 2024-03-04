666011663b02740dd291200052d621fd5812506f
이한주 <leehanju@ihanjuui-noteubug.local>
2024-03-04 22:44:38 +0900

    main page

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
 $(document).ready(function(){
     $(".navi>li>a").on("mouseenter",function(){
         $(".top").css({"background-color":"#fff"});
-        $(".navi>li>a").css({"color":"#000"});
+        $(".top").css({"border-bottom":"1px solid #808080"})
+        $(".submenu").css({"border-left":"1px solid #808080"})
+        // $(".submenu:nth-of-type(1)").css({"border-left":"none"})
+        $(".navi>li>a").css({"color":"#7ed957"});
+        $(".submenu").stop().slideDown(500);
+        $("#bg_menu").stop().slideDown(500);
     });
-    $(".top").on("mouseout",function(){
+    $(".navi").on("mouseleave",function(){
         $(".top").css({"background-color":"transparent"});
+        $(".top").css({"border-bottom":"transparent"})
         $(".navi>li>a").css({"color":"#fff"});
+        $(".submenu").stop().slideUp(300);
+        $("#bg_menu").stop().slideUp(300);
     });
+
+
 });
