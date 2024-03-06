d2df2fba8515a772bf2c82471d5d2f3515b733d6
Marco-Lee <marco000703@naver.com>
2024-03-06 16:42:48 +0900

    main page

--- js/main.js
+++ js/main.js
@@ -1,20 +1,77 @@
 $(document).ready(function(){
-    $(".navi>li>a").on("mouseenter",function(){
-        $(".top").css({"background-color":"#fff"});
-        $(".top").css({"border-bottom":"1px solid #808080"})
-        $(".submenu").css({"border-left":"1px solid #808080"})
-        // $(".submenu:nth-of-type(1)").css({"border-left":"none"})
-        $(".navi>li>a").css({"color":"#7ed957"});
-        $(".submenu").stop().slideDown(500);
-        $("#bg_menu").stop().slideDown(500);
+    
+    $(".navi>li").on("mouseenter",function(){
+       
+        $(window).on("scroll",function(){
+            var scroll=$(this).scrollTop();                   
+            if(scroll<2){
+                 $(".top").css({"background-color":"white"})
+                
+                 $(".navi>li>a").css({"color":"#000"})
+            }               
+        })
+
+        $(".top").css({"background-color":"#fff"})
+        $(".top").css({"border-bottom":"2px solid #eee"})
+        $(".navi>li>a").css({"color":"#7ed957"})
+
+
+        $(".submenu").stop().slideDown(200);
+        $("#bg_menu").stop().slideDown(200);
     });
-    $(".navi").on("mouseleave",function(){
-        $(".top").css({"background-color":"transparent"});
+
+
+    $(".navi>li").on("mouseleave",function(){
+        $(window).on("scroll",function(){
+            var scroll=$(this).scrollTop();                   
+            if(scroll<2){
+                 $(".top").css({"background-color":"white"})
+                
+                 $(".navi>li>a").css({"color":"#000"})
+            }               
+        })
+        
+        $(".top").css({"background-color":"transparent"})
         $(".top").css({"border-bottom":"transparent"})
-        $(".navi>li>a").css({"color":"#fff"});
-        $(".submenu").stop().slideUp(300);
-        $("#bg_menu").stop().slideUp(300);
+        $(".navi>li>a").css({"color":"#fff"})
+
+        $(".submenu").stop().slideUp(200);
+        $("#bg_menu").stop().slideUp(200);
     });
+
+
+    $(window).on("scroll",function(){
+        var scroll=$(this).scrollTop();
+
+
+        console.log(scroll);
+        // if(scroll=0){
+        //      $(".top").css({"background-color":"transparent"})
+        //      $(".top").css({"border-bottom":"transparent"})
+        //      $(".navi>li>a").css({"color":"#fff"})
+        // }
+
+        if(scroll>300){
+            $(".top").css({"background-color":"#fff"})
+            $(".top").css({"border-bottom":"2px solid #eee"})
+            $(".navi>li>a").css({"color":"#7ed957"});
+        }else{
+            $(".top").css({"background-color":"transparent"})
+            $(".top").css({"border-bottom":"transparent"})
+            $(".navi>li>a").css({"color":"#000"})
+        }
+    })
+
+
+    // $(window).on("scroll",function(){
+    //     var scroll=$(this).scrollTop();
+
+    //     if(scroll=0){                
+    //         $(".top").css({"background-color":"transparent"})
+    //         $(".top").css({"border-bottom":"transparent"})
+    //         $(".navi>li>a").css({"color":"#000"})
+    //     }
+    // });
 
 
 });
